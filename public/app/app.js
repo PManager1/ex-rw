3c3b0290812de9da97f797949e37ac8d90d0d9e9
jp-ca <jp_ca@ymail.com>
2014-10-26 19:10:45 -0700

    redirection to main localhost working if user not allowed at that lcoation

--- public/app/app.js
+++ public/app/app.js
@@ -9,8 +9,32 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
 
   $routeProvider
     .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
-   
+    
+    .when('/admin/users', { templateUrl: '/partials/admin/user-list',
+      controller: 'mvUserListCtrl',
+      resolve: {
+        auth: function  (mvIdentity, $q) {
+          if(mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1){
+            return true; 
+          }else{
+            return $q.reject('not authorized'); 
+          }
+        }
+    }
 });
 
 
+}); 
+
+
+
+angular.module('app').run(function  ($rootScope, $location) {
+  console.log(' calling  run ');
+  $rootScope.$on('$routeChangeError', function  (evt, current, previous, rejection) {
+    if(rejection === 'not authorized'){
+      console.log(' rejection happened !!! ');
+      $location.path('/'); 
+    }
+  })
+})
 
